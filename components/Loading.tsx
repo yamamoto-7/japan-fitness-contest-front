592580b25125226b0d0da01ca2880ea67e4e02af
Kazuki Yamamoto <k.yamamoto2477@gmail.com>
2025-11-13 16:44:34 +0900

    feat: TOP画面にローディング追加

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-        <p className="h-screen w-screen flex justify-center items-center text-sm text-slate-400">読み込み中...</p>
+        <p className="h-screen w-screen text-sm text-slate-400">読み込み中...</p>
     );
 }
 
